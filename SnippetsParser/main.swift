d5616c3e8c7340bb8bdf48c7a879ebf43c631da7
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-05-24 19:15:44 -0300

    Parsing downloaded code from github

--- SnippetsParser/main.swift
+++ SnippetsParser/main.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-func strategy(url: URL, saveUrl: URL) {
+func strategy(sourceUrl: URL, saveUrl: URL) {
     let helper = MacOSHelper()
-    let swiftFiles = helper.recursiveFindSwiftFiles(folder: url, swiftFiles: [URL]() )
+    let swiftFiles = helper.recursiveFindSwiftFiles(folder: sourceUrl, swiftFiles: [URL]() )
     let count = swiftFiles.count
     print("Found \(count) swift file\(count != 1 ? "s" : "")")
     swiftFiles.forEach { (url) in
@@ -33,18 +33,23 @@ func strategy(url: URL, saveUrl: URL) {
     }
 }
 
-func downloadFromGithub() -> Bool {
-    print("Will download from github")
-    guard let githubURL = URL(string: "https://github.com/lucianosky/1000snippets/archive/master.zip"),
-          let documentsUrl = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-    else {
-        return false
+extension Process {
+    
+    private static let gitExecURL = URL(fileURLWithPath: "/usr/bin/git")
+    
+    public func clone(repo: String, path: String) throws {
+        executableURL = Process.gitExecURL
+        arguments = ["clone", repo, path]
+        try run()
     }
-    let destination = documentsUrl.appendingPathComponent("1000snippets.zip")
+    
+}
 
-    if FileManager.default.fileExists(atPath: destination.path) {
+func deleteFileIfExists(path: String) -> Bool {
+    if FileManager.default.fileExists(atPath: path) {
+        print("file exists")
         do {
-            try FileManager.default.removeItem(atPath: destination.path)
+            try FileManager.default.removeItem(atPath: path)
             print("Deleted previous version of 1000snippets")
         }
         catch let error as NSError {
@@ -52,25 +57,29 @@ func downloadFromGithub() -> Bool {
             return false
         }
     }
-        
-    let semaphore = DispatchSemaphore(value: 0)
-    URLSession.shared.downloadTask(with: githubURL, completionHandler: { (location, response, error) in
-        // after downloading your data you need to save it to your destination url
-        guard
-            let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-            let mimeType = response?.mimeType, mimeType.hasPrefix("application/zip"),
-            let location = location, error == nil
-            else { return }
-        do {
-            try FileManager.default.moveItem(at: location, to: destination)
-        } catch {
-            print("Could not save file: \(error)")
-        }
-        print("done!")
-        semaphore.signal()
-    }).resume()
-    print("downloading file...")
-    semaphore.wait()
+    return true
+}
+
+func documentsUrl() -> URL? {
+    //print(try! FileManager.default.url(for: .libraryDirectory, in: .userDomainMask, appropriateFor: nil, create: true))
+    return  try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+}
+
+func gitClone(_ gitSource: String, _ targetFolder: String) -> Bool {
+    guard deleteFileIfExists(path: targetFolder) else {
+        print("error deleting folder")
+        return false
+    }
+    print("Will clone repo")
+    let p = Process()
+    do {
+        try p.clone(repo: gitSource, path: targetFolder)
+        p.waitUntilExit()
+    } catch {
+        print("Error cloning repo")
+        return false
+    }
+    print("Done")
     return true
 }
 
@@ -80,11 +89,17 @@ if CommandLine.argc < 3 {
     exit(1)
 }
 
-_ = downloadFromGithub()
-print("continuando...")
+guard let docsUrl = documentsUrl() else {
+    print("Error fetching documents URL")
+    exit(1)
+}
+let gitSource = "https://github.com/lucianosky/1000Snippets.git"
+let targetFolder = docsUrl.path + "/1000SnippetsAppCode"
+_ = gitClone(gitSource, targetFolder)
 
-let url = URL(fileURLWithPath: CommandLine.arguments[1], isDirectory: true)
+let sourceUrl = URL(fileURLWithPath: targetFolder, isDirectory: true)
+//let sourceUrl = URL(fileURLWithPath: CommandLine.arguments[1], isDirectory: true)
 let saveUrl = URL(fileURLWithPath: CommandLine.arguments[2], isDirectory: true)
-strategy(url: url, saveUrl: saveUrl)
+strategy(sourceUrl: sourceUrl, saveUrl: saveUrl)
 
 print("finished...")
